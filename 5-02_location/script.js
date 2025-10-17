53c21a0b6394b3c180f52f73bafa7e1b36208ec3
yasunaga37 <yasunaga37@gmail.com>
2025-10-17 14:46:02 +0900

    5-02_locationまで

---
+++ 5-02_location/script.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+
+const lang = document.querySelector('html').getAttribute('lang');
+
+if (lang === 'ja') {
+  document.querySelector('option[value="index.html"]').selected = true;
+}else if (lang === 'en') {
+  document.querySelector('option[value="index-en.html"]').selected = true;
+}else if (lang === 'zh') {
+  document.querySelector('option[value="index-zh.html"]').selected = true;
+}
+
+document.getElementById('form').select.onchange = function() {
+  location.href = document.getElementById('form').select.value;
+}
